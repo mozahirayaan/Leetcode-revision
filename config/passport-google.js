0dbe851691d9e66e90881c03829a3fb22c7b3bd8
mozahirayaan <mozahirayaan@gmail.com>
2024-08-07 01:00:55 +0530

    k

--- config/passport-google.js
+++ config/passport-google.js
@@ -1,40 +1,40 @@
 require('dotenv').config();
-const passport =require("passport")
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { UserModel} = require('./database');
+const { UserModel } = require('./database'); // Adjust based on your actual file structure
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: '/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
-   
-    let existingUser = await UserModel.findOne({ username: profile.emails[0].value });
+    try {
+        let existingUser = await UserModel.findOne({ username: profile.emails[0].value });
 
-    if (existingUser) {
-        
-        done(null, existingUser);
-    } else {
-        const newUser = new UserModel({
-            googleid: profile.id,
-            username: profile.emails[0].value,
-            name : profile.displayName ,
-            questions: []
-            // Add other user properties as needed
-        });
-        await newUser.save();
-        done(null, newUser);
+        if (existingUser) {
+            done(null, existingUser);
+        } else {
+            const newUser = new UserModel({
+                googleid: profile.id,
+                username: profile.emails[0].value,
+                name: profile.displayName,
+                questions: []
+            });
+            await newUser.save();
+            done(null, newUser);
+        }
+    } catch (err) {
+        done(err, null);
     }
 }));
 
-
 passport.serializeUser((user, done) => {
-    done(null, { id: user.id, name: user.name, username: user.username, questions: user.questions });
+    done(null, user.id); // Store only user ID
 });
 
-passport.deserializeUser(async (obj, done) => {
+passport.deserializeUser(async (id, done) => {
     try {
-        const user = await UserModel.findById(obj.id);
+        const user = await UserModel.findById(id);
         done(null, user);
     } catch (err) {
         done(err, null);
