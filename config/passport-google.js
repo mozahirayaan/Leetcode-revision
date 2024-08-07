1ef987490e7d44fb2ddb1c1ecd773ef717e19fa3
mozahirayaan <159786215+mozahirayaan@users.noreply.github.com>
2024-08-07 23:44:04 +0530

    Update passport-google.js

--- config/passport-google.js
+++ config/passport-google.js
@@ -1,40 +1,40 @@
 require('dotenv').config();
-const passport = require('passport');
+const passport =require("passport")
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const { UserModel } = require('./database'); // Adjust based on your actual file structure
+const { UserModel} = require('./database');
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: '/google/callback'
+    callbackURL: '/api/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
-    try {
-        let existingUser = await UserModel.findOne({ username: profile.emails[0].value });
 
-        if (existingUser) {
-            done(null, existingUser);
-        } else {
-            const newUser = new UserModel({
-                googleid: profile.id,
-                username: profile.emails[0].value,
-                name: profile.displayName,
-                questions: []
-            });
-            await newUser.save();
-            done(null, newUser);
-        }
-    } catch (err) {
-        done(err, null);
+    let existingUser = await UserModel.findOne({ username: profile.emails[0].value });
+
+    if (existingUser) {
+
+        done(null, existingUser);
+    } else {
+        const newUser = new UserModel({
+            googleid: profile.id,
+            username: profile.emails[0].value,
+            name : profile.displayName ,
+            questions: []
+            // Add other user properties as needed
+        });
+        await newUser.save();
+        done(null, newUser);
     }
 }));
 
+
 passport.serializeUser((user, done) => {
-    done(null, user.id); // Store only user ID
+    done(null, { id: user.id, name: user.name, username: user.username, questions: user.questions });
 });
 
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (obj, done) => {
     try {
-        const user = await UserModel.findById(id);
+        const user = await UserModel.findById(obj.id);
         done(null, user);
     } catch (err) {
         done(err, null);
